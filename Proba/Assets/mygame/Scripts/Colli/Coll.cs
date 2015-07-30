49017fc6f8f42225bb1950ed7868ab3ae50d45e2
chudik47 <chudik-l@yandex.ru>
2015-07-30 21:29:10 +0300

    рендер планети відносно екрану, трабли з гейм овером

--- Proba/Assets/mygame/Scripts/Colli/Coll.cs
+++ Proba/Assets/mygame/Scripts/Colli/Coll.cs
@@ -6,9 +6,21 @@ public class Coll : MonoBehaviour {
 	public Rigidbody2D d;
 	public Rigidbody2D c;
 	public GameObject planet;
-	private float a = 1;
-	private float animat=0;
+	private float a ;
+	public Camera main;
+	public static Vector3 world;
+	public static float asd;
 
+	void Start () {
+		
+		world = Camera.main.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, -6.0f));
+		asd = (world.x * -2);
+		asd = asd / 10;
+		
+		planet.transform.localScale = new Vector2 (asd, asd);
+		a = asd;
+		
+	}
 	void OnTriggerEnter2D(Collider2D other)
 	{
 	
@@ -16,19 +28,16 @@ public class Coll : MonoBehaviour {
 
 		if (other.name == "Bird(Clone)") {
 			a += b.mass / 10;
-			animat=b.mass / 10;
 		}
 
 		if( other.name =="Bird1(Clone)"){
 				a += d.mass/10;
-				animat=d.mass/10;
 			}
 		if( other.name =="Bird2(Clone)"){
 				a += c.mass/10;
-				animat=c.mass/10;
 			}
 	
 
-			planet.transform.localScale = new Vector2 (a/10, a/10);
+			planet.transform.localScale = new Vector2 (a, a);
 		}
 	}
---
+++ Proba/Assets/mygame/Scripts/Colli/Sizes.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sizes : MonoBehaviour {
+
+	public GameObject planet;
+	public Camera main;
+	public static Vector3 world;
+	public static float asd;
+	
+	void Start () {
+
+		world = Camera.main.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, -6.0f));
+		asd = (world.x * -2);
+
+
+		planet.transform.localScale = new Vector2 (asd / 10, asd / 10);
+	
+	}
+}
