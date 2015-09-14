53595dc2a78d8098ea94e840f5a49e08e14a0c21
Messering <messering@yandex.ru>
2015-09-15 01:31:18 +0300

    End demo version

--- Proba/Assets/mygame/Scripts/Colli/StartGame.cs
+++ Proba/Assets/mygame/Scripts/Colli/StartGame.cs
@@ -15,23 +15,30 @@ public class StartGame : MonoBehaviour
 {
 	public int time;
 	public GUIText timer;
+	public GameObject text;
+	public GameObject Menu;
 	
 	void Start()
 	{
+		Menu.SetActive (false);
 		StartCoroutine (countdown());
 	}
 	
 	IEnumerator countdown()
 	{
-		while (time > 0)
+		while (time > -2)
 		{
 			yield return new WaitForSeconds(1);
 			
 			timer.text = time.ToString();
 			
 			time -= 1;
+			if(time<0){timer.text = "Start";}
 		}
 		
-		timer.text = "Blast Off!";
+		//timer.text = "Start";
+		text.SetActive (false);
+		timer.text = "";
+		Menu.SetActive (true);
 	}
 }
